663dda5078b082a66cc626c94b4f325a20d05e57
ridhoalpian <ridhoalfian8733@gmail.com>
2023-10-30 17:39:04 +0700

    Splash Screen Animation

--- app/src/main/java/com/example/teamapp/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/teamapp/ui/SplashScreenActivity.kt
@@ -3,7 +3,9 @@ package com.example.teamapp.ui
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.activity.ComponentActivity
+import com.example.teamapp.R
 import com.example.teamapp.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : ComponentActivity() {
@@ -14,19 +16,23 @@ class SplashScreenActivity : ComponentActivity() {
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.root.startAnimation(fadeIn)
+
         val sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
         val isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false)
 
         Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            if (isLoggedIn) {
-                // Pengguna telah login, arahkan ke MainActivity
-                startActivity(Intent(this, MainActivity::class.java))
-            } else {
-                // Pengguna belum login, arahkan ke LoginActivity
-                startActivity(Intent(this, LoginActivity::class.java))
-            }
-            finish()
-        }, 3000)
+            val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+            binding.root.startAnimation(fadeOut)
+                if (isLoggedIn) {
+                    // Pengguna telah login, arahkan ke MainActivity
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    // Pengguna belum login, arahkan ke LoginActivity
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+                finish()
+        }, 2500)
     }
 }
