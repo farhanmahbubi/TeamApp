bd715f88154473219077e70fdc3d6113f4b232c4
ridhoalpian <ridhoalfian8733@gmail.com>
2023-11-01 08:29:38 +0700

    OnBoarding Motion Layout Page

--- app/src/main/java/com/example/teamapp/ui/OnBoarding.kt
+++ app/src/main/java/com/example/teamapp/ui/OnBoarding.kt
@@ -17,28 +17,26 @@ import com.example.teamapp.databinding.FragmentOnBoardingBinding
 import com.example.teamapp.databinding.LoginActivityBinding
 
 class OnBoarding : AppCompatActivity() {
+
     private lateinit var binding: FragmentOnBoardingBinding
-//    private lateinit var sharedPreferences: SharedPreferences
+
+    companion object {
+        private var isFirstRun = true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//
-//        // Initialize SharedPreferences
-//        sharedPreferences = getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
-//
-//        // Check if onboarding has already been shown
-//        if (sharedPreferences.getBoolean("isFirstRun", true)) {
-//            // Onboarding has not been shown, navigate to onboarding
-//            with(sharedPreferences.edit()) {
-//                putBoolean("isFirstRun", false)
-//                apply()
-//            }
-//        } else {
-//            // Onboarding has already been shown, navigate to login activity
-//            startActivity(Intent(this, LoginActivity::class.java))
-//            finish() // Finish current activity to prevent returning to onboarding
-//        }
+
+        if (isFirstRun) {
+            // First run, show onboarding
+            isFirstRun = false
+        } else {
+            // Not the first run, go directly to Login
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish() // Finish current activity to prevent returning to onboarding
+        }
 
         binding.button.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
@@ -60,5 +58,4 @@ class OnBoarding : AppCompatActivity() {
         }
         supportActionBar?.hide()
     }
-
 }
