011af2e8332cf50bee22a5b9df33976f20dbe071
ridhoalpian <ridhoalfian8733@gmail.com>
2023-11-02 12:45:21 +0700

    Update Home Detail

--- app/src/main/java/com/example/teamapp/home/Home.kt
+++ app/src/main/java/com/example/teamapp/home/Home.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.teamapp.R
 import com.example.teamapp.databinding.FragmentHomeBinding
 import com.example.teamapp.detail.Detaill
 import com.example.teamapp.model.ResponseUserGithub
@@ -31,12 +32,17 @@ class Home : Fragment() {
 
         // Set up RecyclerView
         adapter = UserAdapter(mutableListOf()) { user ->
-            val intent = Intent(requireContext(), Detaill::class.java).apply {
-                putExtra("username", user.login)
-            }
-            startActivity(intent)
-        }
+            val detailFragment = Detaill()
+            val bundle = Bundle()
+            bundle.putString("username", user.login)
+            detailFragment.arguments = bundle
 
+            val transaction = parentFragmentManager.beginTransaction() // Menggunakan parentFragmentManager di dalam Fragment
+            transaction.replace(R.id.frame_layout, detailFragment) // Ganti R.id.fragment_container dengan ID wadah fragmen Anda
+            transaction.addToBackStack(null) // Opsional, untuk menambahkan transaksi ke back stack
+            transaction.commit()
+        }
+        
         binding.recycleView.layoutManager = LinearLayoutManager(requireContext())
         binding.recycleView.setHasFixedSize(true)
         binding.recycleView.adapter = adapter
