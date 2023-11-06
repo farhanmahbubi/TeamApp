8b8e0770dfe95edd35ef568bd6a2effb7934cbfd
ridhoalpian <ridhoalfian8733@gmail.com>
2023-11-06 14:51:40 +0700

    Anti Error (Favorit)

--- app/src/main/java/com/example/teamapp/ui/Favorite.kt
+++ app/src/main/java/com/example/teamapp/ui/Favorite.kt
@@ -1,64 +1,19 @@
 package com.example.teamapp.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.teamapp.UserAdapter
-import com.example.teamapp.database.DbModule
-import com.example.teamapp.databinding.FragmentFavoriteBinding
-import com.example.teamapp.detail.DetailActivity
-import com.example.teamapp.detail.Detaill
-import com.example.teamapp.home.UserAdapter
+import com.example.teamapp.R
+
 
 class Favorite : Fragment() {
-    private lateinit var binding: FragmentFavoriteBinding
-    private val adapter by lazy {
-        UserAdapter { user ->
-            context?.let {
-                Intent(it, Detaill::class.java).apply {
-                    putExtra("item", user)
-                    it.startActivity(this)
-                }
-            }
-        }
-    }
-
-    private val viewModel by viewModels<FavoriteViewModel> {
-        FavoriteViewModel.Factory(DbModule(requireContext()))
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.rvFavorite.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvFavorite.adapter = adapter
-
-        viewModel.getUserFavorite().observe(viewLifecycleOwner) {
-            adapter.setData(it)
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                activity?.finish()
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 }
